87c824264604700fdb58925154485e01b83cc37f
Nguyễn Mạnh Đức <nguyenduc2005qo@gmail.com>
2025-09-17 21:21:11 +0700

    set up project

---
+++ src/main/java/com/manhduc205/ezgear/EzgearApplication.java
@@ -0,0 +1,13 @@
+package com.manhduc205.ezgear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzgearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EzgearApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manhduc205/ezgear/EzgearApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manhduc205.ezgear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzgearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
