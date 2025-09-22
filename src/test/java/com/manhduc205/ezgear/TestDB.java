56896896eb19082f043f97f9e116f006472124e7
Nguyễn Mạnh Đức <nguyenduc2005qo@gmail.com>
2025-09-23 00:27:20 +0700

    update pj uploadimage Cloudinary

--- src/test/java/com/manhduc205/ezgear/TestDB.java
+++ src/test/java/com/manhduc205/ezgear/TestDB.java
@@ -2,17 +2,22 @@ package com.manhduc205.ezgear;
 
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+
 @Component
 public class TestDB {
+    @Value("${CLOUDINARY_CLOUD_NAME}")
+    private static String cloudName;
     public static void main(String[] args) {
         PasswordEncoder encoder = new BCryptPasswordEncoder();
         System.out.println(encoder.encode("123456"));
+        System.out.println("Cloudinary Cloud Name = " + cloudName);
     }
 
 
