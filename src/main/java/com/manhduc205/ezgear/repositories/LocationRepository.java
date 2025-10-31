df200dd71c1dfe2bfd5c872fd0de68917ce45d0b
Nguyễn Mạnh Đức <nguyenduc2005qo@gmail.com>
2025-10-31 23:45:19 +0700

    update GHN config

---
+++ src/main/java/com/manhduc205/ezgear/repositories/LocationRepository.java
@@ -0,0 +1,7 @@
+package com.manhduc205.ezgear.repositories;
+
+import com.manhduc205.ezgear.models.Location;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LocationRepository extends JpaRepository<Location, Integer> {
+}
